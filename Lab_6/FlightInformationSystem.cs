ddbcaafa484eacbab5318097a95c8bcd861e3bca
Mix Egor <egormix58@gmail.com>
2024-09-18 13:41:42 +0300

    Годину розбиравсая як це зробити

--- Lab_6/FlightInformationSystem.cs
+++ Lab_6/FlightInformationSystem.cs
@@ -1,44 +1,47 @@
-﻿using Lab_6;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
-public class FlightInformationSystem
+namespace Lab_6
 {
-    public List<Flight> Flights { get; private set; }
 
-    public FlightInformationSystem(string jsonFilePath)
+    public class FlightInformationSystem
     {
-        LoadFlightsFromJson(jsonFilePath);
-    }
+        public List<Flight> Flights { get; private set; }
 
-    private void LoadFlightsFromJson(string jsonFilePath)
-    {
-        try
+        public FlightInformationSystem(string jsonFilePath)
         {
-            var jsonData = File.ReadAllText(jsonFilePath);
-            Flights = JsonConvert.DeserializeObject<List<Flight>>(jsonData);
+            LoadFlightsFromJson(jsonFilePath);
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error loading flight data: {ex.Message}");
-            Flights = new List<Flight>();
-        }
-    }
 
-    public void DisplayFlights()
-    {
-        if (Flights != null && Flights.Count > 0)
+        private void LoadFlightsFromJson(string jsonFilePath)
         {
-            foreach (var flight in Flights)
+            try
             {
-                Console.WriteLine(flight.ToString());
+                var jsonData = File.ReadAllText(jsonFilePath);
+                Flights = JsonConvert.DeserializeObject<List<Flight>>(jsonData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading flight data: {ex.Message}");
+                Flights = new List<Flight>();
             }
         }
-        else
+
+        public void DisplayFlights()
         {
-            Console.WriteLine("No flights available.");
+            if (Flights != null && Flights.Count > 0)
+            {
+                foreach (var flight in Flights)
+                {
+                    Console.WriteLine(flight.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("No flights available.");
+            }
         }
     }
 }
--- Lab_6/FlightQueryHandler.cs
+++ Lab_6/FlightQueryHandler.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace LB6
+namespace Lab_6
 {
+
     public class FlightQueryHandler
     {
+        private FlightInformationSystem flightSystem;
 
+        public FlightQueryHandler(FlightInformationSystem flightSystem)
+        {
+            this.flightSystem = flightSystem;
+        }
+
+        public List<Flight> FindFlightsByNumber(string flightNumber)
+        {
+            return flightSystem.Flights.Where(f => f.FlightNumber.Equals(flightNumber, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public List<Flight> FindFlightsByStatus(FlightStatus status)
+        {
+            return flightSystem.Flights.Where(f => f.Status == status).ToList();
+        }
+
+        public List<Flight> FindFlightsByAirline(string airline)
+        {
+            return flightSystem.Flights.Where(f => f.Airline.Equals(airline, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public List<Flight> FindFlightsByDestination(string destination)
+        {
+            return flightSystem.Flights.Where(f => f.Destination.Equals(destination, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public List<Flight> FindFlightsByDepartureTime(DateTime departureTime)
+        {
+            return flightSystem.Flights.Where(f => f.DepartureTime == departureTime).ToList();
+        }
+
+        public List<Flight> FindFlightsByDate(DateTime date)
+        {
+            return flightSystem.Flights.Where(f => f.DepartureTime.Date == date.Date || f.ArrivalTime.Date == date.Date).ToList();
+        }
+
+        public List<Flight> FindFlightsInTimeRange(DateTime startTime, DateTime endTime)
+        {
+            return flightSystem.Flights.Where(f => (f.DepartureTime >= startTime && f.DepartureTime <= endTime) ||
+                                                   (f.ArrivalTime >= startTime && f.ArrivalTime <= endTime)).ToList();
+        }
+
+        public void DisplayFlights(List<Flight> flights)
+        {
+            if (flights.Any())
+            {
+                foreach (var flight in flights)
+                {
+                    Console.WriteLine(flight.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("No matching flights found.");
+            }
+        }
     }
 }
+
