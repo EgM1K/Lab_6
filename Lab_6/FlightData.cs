088b3085243b4aa5fcc78dc0a3fe32be466acdba
Mix Egor <egormix58@gmail.com>
2024-09-25 10:36:28 +0300

    Забув тут додати коміт для пояснення

--- Lab_6/FlightData.cs
+++ Lab_6/FlightData.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 namespace Lab_6
 {
+    // Цей клас служить для коректної десеріалізації JSON у об'єкт, який містить список рейсів
     public class FlightData
     {
         [JsonProperty("flights")]
